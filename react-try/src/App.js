aa76c23be1a2028401546a97f969a3922c4947e5
Philippe <kamokamophilippe13@gmail.com>
2022-11-02 12:24:14 -0500

    complete tech

--- react-try/src/App.js
+++ react-try/src/App.js
@@ -1,41 +1,61 @@
+import _ from 'lodash';
 import { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from "axios";
 
 function App() {
 
-  const [item, setItem] = useState("");
-  const [saveItem, setSaveItem] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [sortBy, setSortBy] = useState('');
 
-  const getItem = () => {
+  const getData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(({ data }) => {
+      console.log(data);
+      setUsers(data)
+    })
+  };
 
-    const items = {
-      id: Math.floor(Math.random() * 100),
-      value: item
-    }
-    setSaveItem(oldList => [...oldList, items])
-    setItem("");
+  useEffect(() => {
+    getData()
+  }, [])
 
+  let sortedUsers = users;
+  if (sortBy === 'name') {
+    sortedUsers = _.sortBy(users, 'name');
+  } else if (sortBy === 'companyName') {
+    sortedUsers = _.sortBy(users, 'company.name');
   }
-  function deleteItem(id) {
-    const deleteArray = saveItem.filter(item => item.id !== id)
-    setSaveItem(deleteArray);
+
+  const Row = ({ person }) => {
+    const [isRed, setIsRed] = useState(false)
+    return (
+      <tr style={{ backgroundColor: isRed ? "red" : "transparent" }}>
+        <td>{person.name}</td>
+        <td><a href={`https://${person.website}`}>{person.website}</a></td>
+        <td>{person.company.name}</td>
+        <td><input type="checkbox" onClick={() => setIsRed(!isRed)} /></td>
+      </tr>
+    )
   }
 
   return (
-    <div>
-      <input type="text" placeholder='Enter item' value={item} onChange={(e) => setItem(e.target.value)} />
-      <button onClick={getItem}>submit</button>
-      <ul>
-        {saveItem.map(item => {
-          return (
-            <li key={item.id}>{item.value}<button onClick={() => deleteItem(item.id)}>delete</button></li>
-          )
-        }
-        )}
+    <div >
+      <table >
+        <thead>
+          <tr >
 
-      </ul>
-
+            <th onClick={() => setSortBy('name')}>Name</th>
+            <th>website</th>
+            <th onClick={() => setSortBy('companyName')}>Company</th>
+            <th scope='col'>
+              <input type="checkbox" />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedUsers.map((person, index) => <Row key={index} person={person} />)}
+        </tbody>
+      </table>
     </div>
   )
 }
